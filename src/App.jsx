a40e04eaeaff82bff59a029f619dd05b3e6097c5
MinJeng22 <chiaminjeng@outlook.com>
2026-04-18 16:08:06 +0800

    1.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Nav           from "./components/Nav";
-import ContactModal  from "./components/ContactModal";
-import Hero          from "./components/Hero";
-import Stats         from "./components/Stats";
-import Services      from "./components/Services";
-import CaseStudies   from "./components/CaseStudies";
-import Products      from "./components/Products";
-import Partners      from "./components/Partners";
-import Careers       from "./components/Careers";
-import Footer        from "./components/Footer";
-import BackToTop     from "./components/BackToTop";
+import Nav from "./components/Nav";
+import ContactModal from "./components/ContactModal";
+import Hero from "./components/Hero";
+import Stats from "./components/Stats";
+import Services from "./components/Services";
+import CaseStudies from "./components/CaseStudies";
+import Products from "./components/Products";
+import Partners from "./components/Partners";
+import Careers from "./components/Careers";
+import Footer from "./components/Footer";
+import BackToTop from "./components/BackToTop";
 
 import AutoCountAccountingPage from "./pages/products/AutoCountAccounting";
-import Sales2DOPage            from "./pages/apps/Sales2DO";
+import Sales2DOPage from "./pages/apps/Sales2DO";
+import KSLOmniPage from "./pages/KSLOmni";
 
 import "./styles/global.css";
 
@@ -43,9 +44,10 @@ export default function App() {
         <Nav onContact={openContact} />
 
         <Routes>
-          <Route path="/"                              element={<Home onContact={openContact} />} />
+          <Route path="/" element={<Home onContact={openContact} />} />
           <Route path="/products/autocount-accounting" element={<AutoCountAccountingPage onContact={openContact} />} />
-          <Route path="/apps/sales2do"                 element={<Sales2DOPage onContact={openContact} />} />
+          <Route path="/apps/sales2do" element={<Sales2DOPage onContact={openContact} />} />
+          <Route path="/omni" element={<KSLOmniPage onContact={openContact} />} />
         </Routes>
 
         <ContactModal open={modalOpen} onClose={() => setModalOpen(false)} />
