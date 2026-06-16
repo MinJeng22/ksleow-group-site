0ffd29719368206d8d62d8a6bd9e723723696795
Chia Min Jeng <ChiaMinJeng@outlook.com>
2026-06-16 17:43:06 +0800

    build: configure Vite to output fixed filename for SSR entry point

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
   plugins: [react()],
   assetsInclude: ["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.svg", "**/*.gif"],
-});
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: isSsrBuild ? "entry-server.js" : "assets/[name]-[hash].js",
+      },
+    },
+  },
+}));
